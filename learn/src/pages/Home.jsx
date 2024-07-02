968820715cd05402f6b030ded12cd937db06a4ab
DLSiem <pdlsiem17@gmail.com>
2024-07-02 23:55:44 +0530

    cleaning

--- learn/src/pages/Home.jsx
+++ learn/src/pages/Home.jsx
@@ -1,20 +1,13 @@
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-xl font-bold text-center ">Home</h1>
 
-      <p>
+      <p className="mx-auto md:w-2/4 bg-slate-300 p-2">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem debitis
         optio consectetur officia itaque voluptatem, sed deleniti aperiam
         aliquid eos laudantium quasi, corporis consequatur soluta, vel nulla
         adipisci reprehenderit animi.{" "}
-      </p>
-
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
-        perspiciatis dolorem nulla quaerat, vero rerum nam excepturi vitae
-        nostrum suscipit quas eligendi neque odio quisquam voluptates
-        consectetur! Praesentium, ex nemo.
       </p>
     </div>
   );
