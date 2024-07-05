d00295b57028e4067fd044048117ea1fc59af743
DLSiem <pdlsiem17@gmail.com>
2024-07-06 03:24:58 +0530

    useRouteLoaderData() implemented

--- practice-routing/frontend/src/pages/EditEventPage.jsx
+++ practice-routing/frontend/src/pages/EditEventPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import EventForm from "../components/EventForm";
+import { useRouteLoaderData } from "react-router-dom";
 
 const EditEventPage = () => {
-  return (
-    <div>
-      <h1>Edit Event Page</h1>
-    </div>
-  );
+  const data = useRouteLoaderData("event-detail");
+
+  return <EventForm event={data.event} />;
 };
 
 export default EditEventPage;
