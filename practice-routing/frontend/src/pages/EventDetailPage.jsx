0c64ce198a0e099d0a8823db395ccb1aaa9a2604
DLSiem <pdlsiem17@gmail.com>
2024-07-06 02:47:58 +0530

    complete rendering item details

--- practice-routing/frontend/src/pages/EventDetailPage.jsx
+++ practice-routing/frontend/src/pages/EventDetailPage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import EventItem from "../components/EventItem";
-import { json } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 const EventDetailPage = () => {
-  return <EventItem />;
+  const data = useLoaderData();
+
+  return <EventItem event={data.event} />;
 };
 
 export default EventDetailPage;
 
 export async function loader({ request, params }) {
   const id = params.eventId;
-  const response = await fetch("http://localhost:8080/events" + id);
+  const response = await fetch("http://localhost:8080/events/" + id);
   if (!response.ok) {
     return json({ message: "Failed to fetch data" }, { status: 500 });
   } else {
