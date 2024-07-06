70ea7872bfe7b7d2e07bcde2ac1047429cd09a9a
DLSiem <pdlsiem17@gmail.com>
2024-07-06 15:41:37 +0530

    using defer to improve ui

--- practice-routing/frontend/src/pages/EventsPage.jsx
+++ practice-routing/frontend/src/pages/EventsPage.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { useLoaderData, json } from "react-router-dom";
+import React, { Suspense } from "react";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 const EventsPage = () => {
-  const events = useLoaderData();
-  return <EventsList events={events} />;
+  const { events } = useLoaderData();
+  console.log(events);
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading.....</p>}>
+      <Await resolve={events}>
+        {(loadedEvent) => <EventsList events={loadedEvent} />}
+      </Await>
+    </Suspense>
+  );
 };
 
 export default EventsPage;
 
-export const loader = async () => {
+const eventLoader = async () => {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     return json({ message: "Failed to fetch data" }, { status: 500 });
@@ -18,4 +25,10 @@ export const loader = async () => {
     const resData = await response.json();
     return resData.events;
   }
+};
+
+export const loader = () => {
+  return defer({
+    events: eventLoader(),
+  });
 };
