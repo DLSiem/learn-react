029f1d0083b5107b48b2fefa474f0941f9ebdc9e
DLSiem <pdlsiem17@gmail.com>
2024-07-07 14:56:29 +0530

    convention state management

--- refPortal/src/components/Player.jsx
+++ refPortal/src/components/Player.jsx
@@ -1,10 +1,27 @@
+import React, { useState } from "react";
+
 export default function Player() {
+  const [submit, setSubmit] = useState(false);
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setSubmit(false);
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmit(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submit ? name : ""}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <form onSubmit={handleSubmit}>
+          <input value={name} onChange={handleChange} type="text" />
+          <button>Set Name</button>
+        </form>
       </p>
     </section>
   );
